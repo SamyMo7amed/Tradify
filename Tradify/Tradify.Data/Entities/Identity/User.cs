b6023a0175a41585969ab62176bcbd25c7070026
samy <samysamyromioromio@gmail.com>
2025-12-02 13:21:55 +0200

    Add tables in Identity and context in infrastructure

---
+++ Tradify/Tradify.Data/Entities/Identity/User.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkCore.EncryptColumn.Attribute;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Tradify.Data.Helpers;
+
+namespace Tradify.Data.Entities.Identity
+{
+    public  class User : IdentityUser<int>
+    {
+
+        public User() 
+        {
+            UserRefreshTokens = new HashSet<UserRefreshToken>();
+        }
+   
+        public UserName FullName { get; set; }
+
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+        [EncryptColumn]
+        public string? Code { get; set; }
+       // [InverseProperty(nameof(UserRefreshToken.user))]
+        public virtual ICollection<UserRefreshToken> UserRefreshTokens { get; set; }
+
+    }
+}
---
+++ Tradify/Tradify.Infrastructure/InfrastrucureBases/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tradify.Infrastructure.InfrastrucureBases
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(ICollection<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbContextTransaction BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CommitAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRangeAsync(ICollection<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetTableAsTracking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetTableNoTracking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RollBackAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRangeAsync(ICollection<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
