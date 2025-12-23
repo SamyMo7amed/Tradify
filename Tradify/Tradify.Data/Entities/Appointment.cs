812e247a6b0866ec4d3f15781dd551728a58d7d4
salspil <salsabilami598@gmail.com>
2025-12-23 07:16:54 +0200

    add Appoiment,StoreBooking

---
+++ Tradify/Tradify.Data/Entities/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Tradify.Data.Entities.Identity;
+
+namespace Tradify.Data.Entities
+{
+    public  class Appointment
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [ForeignKey(nameof(CustomerId))]
+        public int CustomerId { get; set; }
+        public User? Customer { get; set; }
+
+
+        [ForeignKey(nameof(InstructorId))]
+        public int InstructorId { get; set; }
+        public User? Instructor { get; set; }
+
+
+
+        [ForeignKey(nameof(StoreBookingId))]
+
+        public int StoreBookingId { get; set; }
+        public StoreBooking? StoreBooking { get; set; }
+
+    }
+}
---
+++ Tradify/Tradify.Data/Entities/StoreBooking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Tradify.Data.Entities.Identity;
+
+namespace Tradify.Data.Entities
+{
+    public class StoreBooking
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(StoreId))]
+        public int StoreId { get; set; }
+        public Stores Store { get; set; }
+
+        [ForeignKey(nameof(InstructorId))]
+        public int InstructorId { get; set; }
+        public User Instructor { get; set; }
+
+        public virtual ICollection<Appointment>? Appointments { get; set; }
+    }
+}
