d38707d47958699bfc7dce9ec4f217de9b783dc4
samy <samysamyromioromio@gmail.com>
2026-01-21 16:32:00 +0200

    Add Queries

---
+++ Tradify/Tradify.Core/Features/User/Queries/Models/GetUserByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradify.Core.Bases;
+
+namespace Tradify.Core.Features.User.Queries.Models
+{
+    public class GetUserByIdQuery :  IRequest<Response<GetUserByIdQuery>>
+    {
+
+        public int Id { get; set; }
+        public GetUserByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
---
+++ Tradify/Tradify.Core/Features/User/Queries/Models/GetUserPaginationQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradify.Core.Bases;
+
+namespace Tradify.Core.Features.User.Queries.Models
+{
+    public class GetUserPaginationQuery : IRequest<Response<GetUserPaginationQuery>>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
