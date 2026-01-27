5d3eb970ff00f7975f5cd6ec2311b4e7de743505
samy <samysamyromioromio@gmail.com>
2026-01-27 15:46:33 +0200

    create appcontrollerBase

---
+++ Tradify/Tradify/Bases/AppControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Tradify.Core.Bases;
+
+namespace Tradify.Bases
+{
+    [ApiController]
+    public class AppControllerBase : ControllerBase
+    {
+        private  IMediator _meduator;
+        protected IMediator Mediator => _meduator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        public ObjectResult NewResult<T>(Response<T> response)
+        {
+            return response.StatusCode switch
+            {
+                HttpStatusCode.OK => Ok(response),
+                HttpStatusCode.Created => Created(string.Empty, response),
+                HttpStatusCode.Unauthorized => Unauthorized(response),
+                HttpStatusCode.BadRequest => BadRequest(response),
+                HttpStatusCode.NotFound => NotFound(response),
+                HttpStatusCode.Accepted => Accepted(response),
+                HttpStatusCode.UnprocessableEntity => UnprocessableEntity(response),
+                _ => BadRequest(response)// default case
+            };
+        }
+
+    }
+}
